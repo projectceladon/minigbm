0c07f404ee65ea502466b9d6db937e3ea7517a93
Jason Macnak <natsu@google.com>
2021-09-27 14:19:10 -0700

    gralloc0: Fix sign comparison
    ... to address:
    
    external/minigbm/cros_gralloc/gralloc0/gralloc0.cc:284:26: error:
    comparison of integers of different signs: 'uint32_t' (aka 'unsigned
    int') and 'int' [-Werror,-Wsign-compare]
    
    BUG=b:201325625
    TEST=build inside of AOSP
    
    Change-Id: Ie4f93b8dd82752e6dcf1234627e4d25756af2736
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/minigbm/+/3188653
    Reviewed-by: Yiwei Zhang <zzyiwei@chromium.org>
    Tested-by: Jason Macnak <natsu@google.com>
    Auto-Submit: Jason Macnak <natsu@google.com>
    Commit-Queue: Jason Macnak <natsu@google.com>

--- cros_gralloc/gralloc0/gralloc0.cc
+++ cros_gralloc/gralloc0/gralloc0.cc
@@ -281,7 +281,7 @@ static int gralloc0_perform(struct gralloc_module_t const *module, int op, ...)
 		memset(info, 0, sizeof(*info));
 		info->drm_fourcc = drv_get_standard_fourcc(hnd->format);
 		info->num_fds = hnd->num_planes;
-		for (uint32_t i = 0; i < info->num_fds; i++)
+		for (int i = 0; i < info->num_fds; i++)
 			info->fds[i] = hnd->fds[i];
 
 		ret = mod->driver->resource_info(handle, strides, offsets, &format_modifier);
