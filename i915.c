00fc577babd6bae5e01645d47bb03b870786c73e
Sushma Venkatesh Reddy <sushma.venkatesh.reddy@intel.com>
2023-04-14 09:04:37 -0700

    minigbm: Use clflushopt if supported
    Use clflushopt if it is supported instead of clflush. clflushopt is more
    optimized and suggested by the HW engineers. It has improved MTL unmap
    operation times significantly.
    
    BUG=b:274641129
    TEST=camera preview should be minimum of 30fps
    
    Tracked-On: OAM-131309
    Signed-off-by: Nirmoy Das <nirmoy.das@intel.com>
    Signed-off-by: Sushma Venkatesh Reddy <sushma.venkatesh.reddy@intel.com>
    Change-Id: I9bf7e355d5ebdfb1647c578173123bb73db5571d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/minigbm/+/4382538
    Reviewed-by: Dominik Behr <dbehr@chromium.org>
    Signed-off-by: chenyanxzhu <chenyanx.zhu@intel.com>

--- i915.c
+++ i915.c
@@ -448,9 +448,14 @@ static void i915_clflush(void *start, size_t size)
 
 	__builtin_ia32_mfence();
 	while (p < end) {
+#if defined(__CLFLUSHOPT__)
+		__builtin_ia32_clflushopt(p);
+#else
 		__builtin_ia32_clflush(p);
+#endif
 		p = (void *)((uintptr_t)p + I915_CACHELINE_SIZE);
 	}
+	__builtin_ia32_mfence();
 }
 
 static int gem_param(int fd, int name)
