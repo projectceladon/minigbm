203ab622f0cc1d0b4617da1ecd7e8fb575ebec96
HeYue <yue.he@intel.com>
2023-06-30 14:37:45 +0800

    INTERNAL: Fix CtsHardwareTestCases failures
    Add BO_USE_SW_MASK usage to DRM_FORMAT_RGB888 format,
    this is required by Android R CTS.
    
    Test: android.hardware.cts.HardwareBufferTest#testCreate

--- i915.c
+++ i915.c
@@ -276,6 +276,7 @@ static int i915_add_combinations(struct driver *drv)
 	/* Android CTS tests require this. */
 	drv_add_combination(drv, DRM_FORMAT_BGR888, &metadata_linear, BO_USE_SW_MASK);
 	drv_modify_combination(drv, DRM_FORMAT_ABGR2101010, &metadata_linear, BO_USE_SW_MASK);
+	drv_add_combination(drv, DRM_FORMAT_RGB888, &metadata_linear, BO_USE_SW_MASK);
 
 	/*
 	 * R8 format is used for Android's HAL_PIXEL_FORMAT_BLOB and is used for JPEG snapshots
