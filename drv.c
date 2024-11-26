82369a968b38bdb3013e2e45f8e26a87379ef505
He, Yue <yue.he@intel.com>
2024-11-26 07:07:56 +0000

    Revert "Refactor driver loading code"
    This reverts commit b3e49ae7dbc67d08534db31108799b3732a10998.

--- drv.c
+++ drv.c
@@ -102,7 +102,7 @@ static const struct backend *drv_get_backend(int fd)
 	return NULL;
 }
 
-struct driver *drv_create(int fd, uint64_t gpu_grp_type)
+struct driver *drv_create(int fd)
 {
 	struct driver *drv;
 	int ret;
@@ -118,7 +118,6 @@ struct driver *drv_create(int fd, uint64_t gpu_grp_type)
 
 	drv->fd = fd;
 	drv->backend = drv_get_backend(fd);
-	drv->gpu_grp_type = gpu_grp_type;
 
 	if (!drv->backend)
 		goto free_driver;
@@ -141,14 +140,6 @@ struct driver *drv_create(int fd, uint64_t gpu_grp_type)
 	if (!drv->combos)
 		goto free_mappings;
 
-	if (drv->backend->init) {
-		ret = drv->backend->init(drv);
-		if (ret) {
-			drv_array_destroy(drv->combos);
-			goto free_mappings;
-		}
-	}
-
 	return drv;
 
 free_mappings:
@@ -164,13 +155,20 @@ free_driver:
 	return NULL;
 }
 
-int drv_set_gpu_grp_type(struct driver *drv, uint64_t type)
+int drv_init(struct driver * drv, uint32_t grp_type)
 {
 	int ret = 0;
 	assert(drv);
 	assert(drv->backend);
 
-	drv->gpu_grp_type = type;
+	drv->gpu_grp_type = grp_type;
+	if (drv->backend->init) {
+		ret = drv->backend->init(drv);
+		if (ret) {
+			drv_array_destroy(drv->combos);
+			drv_array_destroy(drv->mappings);
+		}
+	}
 	return ret;
 }
 
@@ -836,15 +834,38 @@ uint32_t drv_get_max_texture_2d_size(struct driver *drv)
 	return UINT32_MAX;
 }
 
-bool drv_is_feature_supported(struct driver * drv, uint64_t feature)
+bool drv_virtpci_with_blob(struct driver * drv)
+{
+        bool ret = false;
+        assert(drv);
+        assert(drv->backend);
+
+        if (drv->backend->virtpci_with_blob) {
+                ret = drv->backend->virtpci_with_blob(drv);
+        }
+        return ret;
+}
+
+bool drv_virtgpu_is_ivshm(struct driver * drv)
+{
+        bool ret = false;
+        assert(drv);
+        assert(drv->backend);
+
+        if (drv->backend->virtgpu_is_ivshm) {
+                ret = drv->backend->virtgpu_is_ivshm(drv);
+        }
+        return ret;
+}
+
+bool drv_is_dgpu(struct driver * drv)
 {
 	bool ret = false;
 	assert(drv);
 	assert(drv->backend);
 
-	if (drv->backend->is_feature_supported) {
-		ret = drv->backend->is_feature_supported(drv, feature);
+	if (drv->backend->is_dgpu) {
+		ret = drv->backend->is_dgpu(drv);
 	}
 	return ret;
 }
-
