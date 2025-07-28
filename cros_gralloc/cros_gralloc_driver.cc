6cf495ecdc9099cb3a2d27a69515cc25e0a24daa
ZhuChenyanX <zhucx@intel.com>
2025-07-28 01:18:23 +0000

    Fix scrcpy fail issue
    Scrcpy needs WC bo, otherwise it'll has mos_bo_create_from_prime_xe
    fail error. The AB24 bo uses VIDEO_ENCODER flag, but no CPU_READ_OFTEN
    flag.
    
    Tracked-On: OAM-133730
    Signed-off-by: ZhuChenyanX <zhucx@intel.com>

--- cros_gralloc/cros_gralloc_driver.cc
+++ cros_gralloc/cros_gralloc_driver.cc
@@ -395,15 +395,15 @@ bool cros_gralloc_driver::get_resolved_format_and_use_flags(
 		resolved_use_flags &= ~BO_USE_SCANOUT;
 		combo = drv_get_combination(drv, resolved_format, descriptor->use_flags);
 	}
-	if (!combo && (descriptor->droid_usage & GRALLOC_USAGE_HW_VIDEO_ENCODER) &&
-	    descriptor->droid_format != HAL_PIXEL_FORMAT_YCbCr_420_888) {
-		// Unmask BO_USE_HW_VIDEO_ENCODER for other formats. They are mostly
-		// intermediate formats not passed directly to the encoder (e.g.
-		// camera). YV12 is passed to the encoder component, but it is converted
-		// to YCbCr_420_888 before being passed to the hw encoder.
-		resolved_use_flags &= ~BO_USE_HW_VIDEO_ENCODER;
-		combo = drv_get_combination(drv, resolved_format, resolved_use_flags);
-	}
+	// if (!combo && (descriptor->droid_usage & GRALLOC_USAGE_HW_VIDEO_ENCODER) &&
+	//     descriptor->droid_format != HAL_PIXEL_FORMAT_YCbCr_420_888) {
+	// 	// Unmask BO_USE_HW_VIDEO_ENCODER for other formats. They are mostly
+	// 	// intermediate formats not passed directly to the encoder (e.g.
+	// 	// camera). YV12 is passed to the encoder component, but it is converted
+	// 	// to YCbCr_420_888 before being passed to the hw encoder.
+	// 	resolved_use_flags &= ~BO_USE_HW_VIDEO_ENCODER;
+	// 	combo = drv_get_combination(drv, resolved_format, resolved_use_flags);
+	// }
 	if (!combo && (descriptor->droid_usage & BUFFER_USAGE_FRONT_RENDERING_MASK)) {
 		resolved_use_flags &= ~BO_USE_FRONT_RENDERING;
 		resolved_use_flags |= BO_USE_LINEAR;
